663487217ad14b530d867bb01f0a84a85fe22af1
ryancyq <ryancyq@gmail.com>
2018-09-15 14:50:51 +0800

    added localization

---
+++ src/Abp.Push.Common/Push/Localization/AbpPushLocalizationConfigurer.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Configuration.Startup;
+using Abp.Localization.Dictionaries;
+using Abp.Localization.Dictionaries.Xml;
+using Abp.Reflection.Extensions;
+
+namespace Abp.Push.Localization
+{
+    internal static class AbpPushLocalizationConfigurer
+    {
+        public static void Configure(ILocalizationConfiguration localizationConfiguration)
+        {
+            localizationConfiguration.Sources.Add(
+                new DictionaryBasedLocalizationSource(
+                    AbpPushConsts.LocalizationSourceName,
+                    new XmlEmbeddedFileLocalizationDictionaryProvider(
+                        typeof(AbpPushCommonModule).GetAssembly(), "Abp.Push.Localization"
+                    )
+                )
+            );
+        }
+    }
+}
