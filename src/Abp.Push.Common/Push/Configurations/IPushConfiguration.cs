ffcbf4ea8e31d301728439ae112f06dd169b73c1
ryancyq <ryancyq@gmail.com>
2018-09-15 15:05:52 +0800

    added push configurations

---
+++ src/Abp.Push.Common/Push/Configurations/IPushConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Abp.Collections;
+using Abp.Push.Providers;
+
+namespace Abp.Push.Configurations
+{
+    /// <summary>
+    /// Used to configure push system.
+    /// </summary>
+    public interface IPushConfiguration
+    {
+        ITypeList<PushDefinitionProvider> DefinitionProviders { get; }
+
+        List<PushServiceProviderInfo> ServiceProviders { get; }
+    }
+}
---
+++ src/Abp.Push.Common/Push/Configurations/PushConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Abp.Collections;
+using Abp.Dependency;
+using Abp.Push.Providers;
+
+namespace Abp.Push.Configurations
+{
+    internal class PushConfiguration : IPushConfiguration, ISingletonDependency
+    {
+        public ITypeList<PushDefinitionProvider> DefinitionProviders { get; private set; }
+
+        public List<PushServiceProviderInfo> ServiceProviders { get; private set; }
+
+        public PushConfiguration()
+        {
+            DefinitionProviders = new TypeList<PushDefinitionProvider>();
+            ServiceProviders = new List<PushServiceProviderInfo>();
+        }
+    }
+}
