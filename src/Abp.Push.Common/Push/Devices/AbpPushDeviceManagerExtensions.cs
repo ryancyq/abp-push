d3a8d6b67ea908cbb99d21bc557e24539b3c7fc4
ryancyq <ryancyq@gmail.com>
2018-09-19 01:38:19 +0800

    updated abp push device manager extensions

---
+++ src/Abp.Push.Common/Push/Devices/AbpPushDeviceManagerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Threading;
+
+namespace Abp.Push.Devices
+{
+    /// <summary>
+    /// Extension methods for <see cref="AbpPushDeviceManager{TDevice}"/>.
+    /// </summary>
+    public static class AbpPushDeviceManagerExtensions
+    {
+        /// <summary>
+        /// Determines whether the specified user has device to be pushed to.
+        /// </summary>
+        /// <param name="manager">The push device manager.</param>
+        /// <param name="user">The user.</param>
+        public static bool CanPush<TDevice>(this AbpPushDeviceManager<TDevice> manager, IUserIdentifier user)
+            where TDevice : AbpPushDevice
+        {
+            return AsyncHelper.RunSync(() => manager.GetCountByUserAsync(user)) > 0;
+        }
+    }
+}
