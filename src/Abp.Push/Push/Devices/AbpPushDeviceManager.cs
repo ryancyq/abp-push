58ef38dbb1c2814a89618da99b288991c9714a6c
ryancyq <ryancyq@gmail.com>
2018-09-21 02:34:47 +0800

    updated abp device implementation

---
+++ src/Abp.Push/Push/Devices/AbpPushDeviceManager.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Push.Configurations;
+
+namespace Abp.Push.Devices
+{
+    public class AbpPushDeviceManager : AbpPushDeviceManager<PushDevice>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbpPushDeviceManager"/> class.
+        /// </summary>
+        protected AbpPushDeviceManager(
+            IPushDeviceStore<PushDevice> deviceStore,
+            IPushConfiguration pushConfiguration
+        ) : base(
+            deviceStore,
+            pushConfiguration
+        )
+        {
+        }
+    }
+}
---
+++ src/Abp.Push/Push/Devices/AbpPushDeviceStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+
+namespace Abp.Push.Devices
+{
+    /// <summary>
+    /// Implements <see cref="IPushDeviceStore"/> using repositories.
+    /// </summary>
+    internal class AbpPushDeviceStore : AbpPushDeviceStore<PushDevice>, IPushDeviceStore, ITransientDependency
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AbpPushDeviceStore"/> class.
+        /// </summary>
+        public AbpPushDeviceStore(
+            IRepository<PushDevice, Guid> deviceRepository
+        ) : base(
+            deviceRepository
+        )
+        {
+        }
+    }
+}
