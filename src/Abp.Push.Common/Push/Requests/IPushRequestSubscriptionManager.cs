23c02d9f46f7842cea5bc0e0d194246172566214
ryancyq <ryancyq@gmail.com>
2018-09-24 02:08:25 +0800

    added push request subscription manager and extensions

---
+++ src/Abp.Push.Common/Push/Requests/IPushRequestSubscriptionManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+
+namespace Abp.Push.Requests
+{
+    /// <summary>
+    /// Used to manage subscriptions for push requests.
+    /// </summary>
+    public interface IPushRequestSubscriptionManager
+    {
+        /// <summary>
+        /// Subscribes to a push request for given user and push request informations.
+        /// </summary>
+        /// <param name="user">User</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        Task SubscribeAsync(IUserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null);
+
+        /// <summary>
+        /// Subscribes to all available push request for given user.
+        /// It does not subscribe entity related push request.
+        /// </summary>
+        /// <param name="user">User.</param>
+        Task SubscribeToAllAvailablePushRequetsAsync(UserIdentifier user);
+
+        /// <summary>
+        /// Unsubscribes from a push request.
+        /// </summary>
+        /// <param name="user">User.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        Task UnsubscribeAsync(IUserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null);
+
+        /// <summary>
+        /// Gets all subscribtions for given push request (including all tenants).
+        /// This only works for single database approach in a multitenant application!
+        /// </summary>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        Task<List<PushRequestSubscription>> GetSubscriptionsAsync(string pushRequestName, EntityIdentifier entityIdentifier = null, int skipCount = 0, int maxResultCount = int.MaxValue);
+
+        /// <summary>
+        /// Gets all subscribtions for given push request.
+        /// </summary>
+        /// <param name="tenantId">Tenant id. Null for the host.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        Task<List<PushRequestSubscription>> GetSubscriptionsAsync(int? tenantId, string pushRequestName, EntityIdentifier entityIdentifier = null, int skipCount = 0, int maxResultCount = int.MaxValue);
+
+        /// <summary>
+        /// Gets subscribed push requests for a user.
+        /// </summary>
+        /// <param name="user">User.</param>
+        Task<List<PushRequestSubscription>> GetSubscribedPushRequestsAsync(IUserIdentifier user, int skipCount = 0, int maxResultCount = int.MaxValue);
+
+        /// <summary>
+        /// Checks if a user subscribed for a push request.
+        /// </summary>
+        /// <param name="user">User.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        Task<bool> IsSubscribedAsync(IUserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null);
+    }
+}
---
+++ src/Abp.Push.Common/Push/Requests/PushRequestSubscriptionManagerExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Abp.Domain.Entities;
+using Abp.Threading;
+
+namespace Abp.Push.Requests
+{
+    public static class PushRequestSubscriptionManagerExtensions
+    {
+        /// <summary>
+        /// Subscribes to a push request.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="user">User.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        public static void Subscribe(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, IUserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null)
+        {
+            AsyncHelper.RunSync(() => pushRequestSubscriptionManager.SubscribeAsync(user, pushRequestName, entityIdentifier));
+        }
+
+        /// <summary>
+        /// Subscribes to all available push requests for given user.
+        /// It does not subscribe entity related push requests.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="user">User.</param>
+        public static void SubscribeToAllAvailablePushRequets(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, UserIdentifier user)
+        {
+            AsyncHelper.RunSync(() => pushRequestSubscriptionManager.SubscribeToAllAvailablePushRequetsAsync(user));
+        }
+
+        /// <summary>
+        /// Unsubscribes from a push request.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="user">User.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        public static void Unsubscribe(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, UserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null)
+        {
+            AsyncHelper.RunSync(() => pushRequestSubscriptionManager.UnsubscribeAsync(user, pushRequestName, entityIdentifier));
+        }
+
+        /// <summary>
+        /// Gets all subscribtions for given push request.
+        /// Can work only for single database approach
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        public static List<PushRequestSubscription> GetSubscriptions(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, string pushRequestName, EntityIdentifier entityIdentifier = null, int skipCount = 0, int maxResultCount = int.MaxValue)
+        {
+            return AsyncHelper.RunSync(() => pushRequestSubscriptionManager.GetSubscriptionsAsync(pushRequestName, entityIdentifier, skipCount, maxResultCount));
+        }
+
+        /// <summary>
+        /// Gets all subscribtions for given push request.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="tenantId">Tenant id. Null for the host.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        public static List<PushRequestSubscription> GetSubscriptions(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, int? tenantId, string pushRequestName, EntityIdentifier entityIdentifier = null, int skipCount = 0, int maxResultCount = int.MaxValue)
+        {
+            return AsyncHelper.RunSync(() => pushRequestSubscriptionManager.GetSubscriptionsAsync(tenantId, pushRequestName, entityIdentifier, skipCount, maxResultCount));
+        }
+
+        /// <summary>
+        /// Gets subscribed push requests for a user.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="user">User.</param>
+        public static List<PushRequestSubscription> GetSubscribedPushRequests(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, IUserIdentifier user, int skipCount = 0, int maxResultCount = int.MaxValue)
+        {
+            return AsyncHelper.RunSync(() => pushRequestSubscriptionManager.GetSubscribedPushRequestsAsync(user, skipCount, maxResultCount));
+        }
+
+        /// <summary>
+        /// Checks if a user subscribed for a push request.
+        /// </summary>
+        /// <param name="pushRequestSubscriptionManager">Push request subscription manager</param>
+        /// <param name="user">User.</param>
+        /// <param name="pushRequestName">Name of the push request.</param>
+        /// <param name="entityIdentifier">entity identifier</param>
+        public static bool IsSubscribed(this IPushRequestSubscriptionManager pushRequestSubscriptionManager, IUserIdentifier user, string pushRequestName, EntityIdentifier entityIdentifier = null)
+        {
+            return AsyncHelper.RunSync(() => pushRequestSubscriptionManager.IsSubscribedAsync(user, pushRequestName, entityIdentifier));
+        }
+    }
+}
