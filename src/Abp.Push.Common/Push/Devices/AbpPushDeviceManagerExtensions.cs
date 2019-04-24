3a369fa1b8b6b9c4769dc7614c0d9369d7273c82
ryancyq <ryancyq@gmail.com>
2019-04-25 01:12:12 +0800

    rename CanPush to HasAny in abp device manager

--- src/Abp.Push.Common/Push/Devices/AbpPushDeviceManagerExtensions.cs
+++ src/Abp.Push.Common/Push/Devices/AbpPushDeviceManagerExtensions.cs
@@ -8,12 +8,12 @@ namespace Abp.Push.Devices
     public static class AbpPushDeviceManagerExtensions
     {
         /// <summary>
-        /// Determines whether the specified user has device to be pushed to.
+        /// Determines whether the specified user has any devices can be pushed to.
         /// </summary>
         /// <param name="manager">The push device manager.</param>
         /// <param name="user">The user.</param>
-        public static bool CanPush<TDevice>(this AbpPushDeviceManager<TDevice> manager, IUserIdentifier user)
-            where TDevice : PushDevice
+        public static bool HasAny<TDevice>(this AbpPushDeviceManager<TDevice> manager, IUserIdentifier user)
+            where TDevice : AbpPushDevice, new()
         {
             return AsyncHelper.RunSync(() => manager.GetCountByUserAsync(user)) > 0;
         }
